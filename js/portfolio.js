3d5257e97164e492010040903c72f4e870878b35
layonthebeech <beecherp@gmail.com>
2017-01-31 13:19:40 -0600

    Boom

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,7 +6,12 @@
 
         $('.section').addClass('invisible');
         $($anchor.attr('href')).removeClass('invisible');
-
+        console.log($anchor.attr('href'))
+        if($anchor.attr('href') !== '#landing') {
+          $('.carousel-text').addClass('invisible');
+        } else {
+          $('.carousel-text').removeClass('invisible');
+        }
     });
     $('body').scrollspy({
         target: '.navbar-fixed-top',
@@ -14,12 +19,18 @@
     });
     if($(window).width() <=599) {
        $('.nav-collapse').removeClass("in");
+       var photos = $('#photos > .indicator > .carousel > .carousel-inner > .item');
+       console.log(photos)
+       photos.eq(0).addClass('active');
+       photos.eq(1).addClass('active');
+       photos.eq(2).addClass('active');
     }
   $( window ).resize(function() {
     console.log('smoll', $(window).width())
     if($(window).width() <=599)  {
     //  $('.carousel-inner').addClass('invisible');
       $('.nav-collapse').removeClass("in");
+
     }
     else {
     //  $('.carousel-inner').removeClass('invisible');
